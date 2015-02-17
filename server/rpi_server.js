50564384a8dcb7410b467e6d096dbbcd72ee05b8
Artjom Siebert <siebertartjom@googlemail.com>
2015-02-17 16:49:19 +0100

    Entries in gitignore file for node.js

--- server/rpi_server.js
+++ server/rpi_server.js
@@ -1,1 +1,11 @@
 /*Start here the nodejs server to define a RESTful API */
+
+var express = require('express');
+var http = require('http'); 
+
+var app = express();
+app.get('*', function (req, res) {
+  res.contentType('text/html');
+  res.send(200, 'Hallo Welt!');
+});
+http.createServer(app).listen(3000);
