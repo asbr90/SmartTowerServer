f96d4042f20b3c2f274ce32ee10120f3037849dd
asbr90 <siebertartjom@googlemail.com>
2015-02-22 21:26:58 +0000

    Extend forecast

--- server/rpi_server.js
+++ server/rpi_server.js
@@ -2,14 +2,33 @@ var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 var net = require('net');
-
+var Forecast = require('forecast');
 var HOST = '127.0.0.1';
 var PORT = 51717;
 var client = new net.Socket(); // connect to Hardware-Layer
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({
+// Initialize
+var forecast = new Forecast({
+  service: 'forecast.io',
+  key: '88e750b2992857ef17c874e812bc4ad0',
+  units: 'celcius', // Only the first letter is parsed
+  cache: true,      // Cache API requests?
+  ttl: {            // How long to cache requests. Uses syntax from moment.js:
+					// http://momentjs.com/docs/#/durations/creating/
+    minutes: 27,
+    seconds: 45
+    }
+});
+ 
+ 
+// Retrieve weather information, ignoring the cache
+forecast.get([50.5833, 8.65], true, function(err, weather) {
+  if(err) return console.dir(err);
+  console.dir(weather);
+});
+
+
+  app.use(bodyParser.urlencoded({
 	extended : true
 }));
 app.use(bodyParser.json());
@@ -18,14 +37,13 @@ var port = process.env.PORT || 3000; // set our port
 
 var router = express.Router(); // get an instance of the express Router
 // middleware to use for all requests
+
 router.use(function(req, res, next) {
 	// do logging
 	console.log('Received Request', req.query);
 	next(); // make sure we go to the next routes and don't stop here
 });
 
-// test route to make sure everything is working (accessed at GET
-// http://localhost:8080/api)
 router.get('/', function(req, res) {
 	res.json({
 		message : 'Smart Towert API!'
@@ -71,16 +89,12 @@ router.get('/hue/:state/:nodeid/:endpoint/:sendmode/:value',
 				res.json({
 					message : 'HueLevel/' + payload
 				});
-			}else if (req.params.state === "saturation") {
+			} else if (req.params.state === "saturation") {
 				client.write('HueSaturation/' + payload);
 				res.json({
 					message : 'HueSaturation/' + payload
 				});
 			}
-
-			// Write a message to the socket as soon as the client is connected,
-			// the
-			// server will receive it as message from the client
 		});
 
 router.get('/hue/:id', function(req, res) {
@@ -101,9 +115,7 @@ router.get('/socket/:nodeid/:endpoint/:sendmode/:value', function(req, res) {
 });
 
 client.connect(PORT, HOST, function() {
-
 	console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-
 });
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
@@ -116,6 +128,10 @@ client.connect(PORT, HOST, function() {
 // Add a 'close' event handler for the client socket
 client.on('close', function() {
 	console.log('Connection closed');
+});
+
+client.on('error', function() {
+	console.log('No available Socket on this port');
 });
 
 // more routes for our API will happen here
